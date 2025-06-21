cc73c1165641a06f0eeb353eaf0c2eeec8ff817e
carlosPin769 <152306109+carlosPin769@users.noreply.github.com>
2025-06-21 19:17:43 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,88 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const Note = require('./models/note')
+
+
+
+app.use(express.json())
+
+
+//app.use(express.static('dist'))
+
+const cors = require('cors')
+app.use(cors())
+let notes = []
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+app.use(requestLogger)
+
+app.get('/', (request, response) => {
+  response.send('<h1>Adios Que te vas!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id)
+  .then((note) => {
+    response.json(note)
+  })
+  .catch((error) => {
+    console.error("Nota no Encontrada: ", error);
+	response.status(204).end();
+  })
+})
+
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3002
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
