b4f5f2b099ce1c713a0e28677db7c116e5a2d866
carlosPin769 <152306109+carlosPin769@users.noreply.github.com>
2025-06-25 11:23:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const Note = require('./models/note')
 
 app.use(express.json())
 
+
 app.use(express.static('dist'))
 
 const cors = require('cors')
@@ -20,7 +21,20 @@ const requestLogger = (request, response, next) => {
   console.log('---')
   next()
 }
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 app.use(requestLogger)
+
+
 
 app.get('/', (request, response) => {
   response.send('<h1>Adios Que te vas!</h1>')
@@ -28,19 +42,30 @@ app.get('/', (request, response) => {
 
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
-    response.json(notes)
+	  response.json(notes)
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/info',(request, response) => {
+	
+	Note.find({}).then(notes => {
+		const notesLen = notes.length
+		const fecha = new Date().toString()
+		const html = `<p>Agenda de Notas tiene informacion de: ${notesLen} Notas</p><p>  ${fecha} </p>`;
+    response.send(html)
+  })
+})
+
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
   .then((note) => {
-    response.json(note)
+	  if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
   })
-  .catch((error) => {
-    console.error("Nota no Encontrada: ", error);
-	response.status(204).end();
-  })
+  .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
@@ -48,7 +73,7 @@ app.delete('/api/notes/:id', (request, response, next) => {
     .then(result => {
       response.status(204).end()
     })
-    .catch(error => next(error))
+	 .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -95,6 +120,13 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
+app.use(errorHandler)
+
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 
 const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
